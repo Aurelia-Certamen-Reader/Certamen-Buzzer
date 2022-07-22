ee7bf9ddbce0507c768a17f278dd556e94f39544
Saffron <saffron.lu.liu@gmail.com>
2022-07-22 12:45:38 -0500

    Better formatting, second dummy question

--- godknows.js
+++ godknows.js
@@ -2,7 +2,8 @@
 let readSpeed = 250
 let buzz = false
 let questionNumber = -1
-const questionBank = [["Pretend this is an actual question! Lorem ipsum dolor sit amet. Yeah, I dunno man.", "Pretend this is an answer"], ["IIIII IIIIIIIIIIIDK"]]
+const questionBank = [["Pretend this is an actual question! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porttitor viverra maximus. Sed bibendum, justo sit amet auctor fringilla, ligula ante pretium eros, at hendrerit nunc lorem faucibus felis. Quisque quis sem vel lectus finibus feugiat id quis magna. Fusce fringilla, nisl non pellentesque vestibulum, ex tellus rutrum tortor, vitae rutrum libero mi in neque. Phasellus aliquet bibendum sem, id volutpat eros ultricies ac. Proin fermentum tellus non erat suscipit malesuada. Sed bibendum semper hendrerit. Phasellus in volutpat leo, vel condimentum erat. Yeah, I dunno man.", "Pretend this is an answer"], 
+["Next question! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porttitor viverra maximus. Sed bibendum, justo sit amet auctor fringilla, ligula ante pretium eros, at hendrerit nunc lorem faucibus felis. Quisque quis sem vel lectus finibus feugiat id quis magna. Fusce fringilla, nisl non pellentesque vestibulum, ex tellus rutrum tortor, vitae rutrum libero mi in neque. Phasellus aliquet bibendum sem, id volutpat eros ultricies ac. Proin fermentum tellus non erat suscipit malesuada. Sed bibendum semper hendrerit. Phasellus in volutpat leo, vel condimentum erat.", "It's a different answer now!"]]
 
 function sleep(time){
     return new Promise(resolve => {setTimeout(resolve, time)});
@@ -11,6 +12,7 @@ function sleep(time){
 async function printQuestion(){
     //Reset
     document.getElementById("answerInput").style.visibility="hidden"
+    document.getElementById('answerInput').value=""
     document.getElementById("answerline").style.visibility="hidden"
     buzz=false
     questionNumber+=1
