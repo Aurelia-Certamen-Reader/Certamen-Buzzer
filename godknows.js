de0237471108aac79eb87d61ed9f92f32ce1c800
Saffron <saffron.lu.liu@gmail.com>
2022-07-22 10:32:16 -0500

    First commit

---
+++ godknows.js
@@ -0,0 +1,31 @@
+//const $ = document.getElementById;
+let readSpeed = 250
+let buzz = false
+let questionNumber = 0
+const questionBank = [["AAAA AAAAAA Aa"], ["IIIII IIIIIIIIIIIDK"]]
+
+function sleep(time){
+    return new Promise(resolve => {setTimeout(resolve, time)});
+}
+
+async function printQuestion(question){
+    let questionText = question.split(" ")
+    document.getElementById("question").innerHTML = ""
+    for (let x of questionText){
+        if (buzz == false){
+            document.getElementById("question").append(x + " ")
+            await sleep(readSpeed);
+        } else{break}
+    }
+}
+
+function endQuestion() {
+    questionNumber+=1
+    buzz=false
+    document.getElementById("answerInput").style.visibility="visible"
+    document.getElementById("answerInput").focus()
+}
+
+//printQuestion("Give the Latin word and its meaning that appears in the mottoes of both the state of New Mexico and the University of Chicago.")
+document.getElementById("answerInput").style.visibility="hidden"
+printQuestion(questionBank[questionNumber][0])
